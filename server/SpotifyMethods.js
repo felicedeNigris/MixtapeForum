d5c8e1c66c86f563f5c13e50d1095cf5a3bb5bb6
Felice DeNigris <felideni@gmail.com>
2016-03-06 22:43:13 -0800

    Styling the input form in the Mix tape container

--- server/SpotifyMethods.js
+++ server/SpotifyMethods.js
@@ -1,13 +1,16 @@
+/* jshint -W033 */
 Meteor.methods({
-  //Spotify call
-  //var spotifyApi = new SpotifyWebApi();
-  // var response = spotifyApi.refreshAccessToken();
+
 
 
   // Get a user's playlists
   getUserPlaylists: function() {
     //Spotify call
-    var spotifyApi = new SpotifyWebApi()
+    var spotifyApi = new SpotifyWebApi({
+      clientId : 'd12a078de127492693230ee1b9a1380e',
+      clientSecret : '051ec17063a54e4e8cc5056b6b7398b1',
+      redirectUri : 'http://localhost:3000/_oauth/spotify?close'
+    })
     //response object
     var userplaylists = spotifyApi.getUserPlaylists(Meteor.user().profile.id,function(err,data){
       if(err){
@@ -32,8 +35,6 @@ Meteor.methods({
     return userplaylists
 
   }//end getUserPlaylists
-
-  //
 
 
 })//end Meteor.methods
