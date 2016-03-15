7f012b2876823b5790b94391bed1296aac51bdc1
EricSSartorius <EricSSartorius@gmail.com>
2016-03-15 14:19:10 -0400

    fixed bug where is-showing class came up more than once

--- client/controllers/mainCtrl.ng.js
+++ client/controllers/mainCtrl.ng.js
@@ -10,33 +10,44 @@ function mainCtrl($scope){
 		//Animate text in hero on load
 		introText.className += ' is-showing';
 
-		
-
 		window.addEventListener("scroll", function(event) {
+			var wScroll = document.body.scrollTop;
+			var botLeft = document.getElementsByClassName('bot-left-intro')[0];
+			var botRight = document.getElementsByClassName('bot-right-intro')[0];
+			var midLeft = document.getElementsByClassName('mid-left-intro')[0];
+			var midRight = document.getElementsByClassName('mid-right-intro')[0];
+			var bot = document.getElementsByClassName('intro-bot')[0];
+			var mid = document.getElementsByClassName('intro-mid')[0];
+			var btn = document.getElementsByClassName('btn')[0];
+			var logo = document.getElementsByClassName('spotify-logo')[0];
+		 	console.log(wScroll);
 
-		var wScroll = document.body.scrollTop;
-		var botLeft = document.getElementsByClassName('bot-left-intro')[0];
-		var botRight = document.getElementsByClassName('bot-right-intro')[0];
-		var btn = document.getElementsByClassName('btn')[0];
-	  // 	console.log (botLeft.scrollHeight);
-	  	// console.log (window.innerHeight);
-	  // 	console.log(botLeft.getBoundingClientRect().top);
-	 	console.log(wScroll);
-	 	console.log(botLeft.getBoundingClientRect().top - window.innerHeight);
-	 	// console.log(botLeft.getBoundingClientRect().top - botLeft.scrollHeight);
-        // console.log("Current offset from the top is " + botLeft.getBoundingClientRect().top + " pixels");
-       	
-		if (document.documentElement.scrollTop || botLeft.getBoundingClientRect().top - window.innerHeight < -50) {
-            botLeft.className += ' is-showing';
-		  	botRight.className += ' is-showing'; 
-		  	console.log('triggered'); 
-         }
-		if(document.documentElement.scrollTop || btn.getBoundingClientRect().top - window.innerHeight < -50) {
-			btn.className += ' is-showing';
-			console.log('btn triggered');
-		}
+	       	//Animate  text on scroll
+	       	if(document.documentElement.scrollTop || midLeft.getBoundingClientRect().top - window.innerHeight < -50) {
+	       		midLeft.classList.add('is-showing');
+			}
+			if(document.documentElement.scrollTop || midRight.getBoundingClientRect().top - window.innerHeight < -100) {
+				midRight.classList.add('is-showing');
+			}
+			if(document.documentElement.scrollTop || botLeft.getBoundingClientRect().top - window.innerHeight < -50) {
+	            botLeft.classList.add('is-showing');
+			  	botRight.classList.add('is-showing'); 
+	         }
+			if(document.documentElement.scrollTop || btn.getBoundingClientRect().top - window.innerHeight < -50) {
+				btn.classList.add('is-showing');
+			}
 
+			//Move triangles in on scroll
+			if(document.documentElement.scrollTop || mid.getBoundingClientRect().top - window.innerHeight < 0) {
 
+			}
+			console.log(wScroll - document.body.clientHeight);
+			//Move logo up on scroll
+			if(document.documentElement.scrollTop || bot.getBoundingClientRect().top - window.innerHeight < 0) {
+				logo.style.transform = 'translate(0px, -'+ wScroll /4 +'%)';
+				// console.log(logo.getBoundingClientRect().top - window.innerHeight);
+				// console.log('triggered');
+			}
     	});
 	}
 }
