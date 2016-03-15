bffabd886a60afb11deaa8fbc60924ddf7444017
Felice DeNigris <felideni@gmail.com>
2016-03-14 22:13:34 -0700

    Directive Modal that has 'Isolative' scope for the delete and pop up modal. The Directive deletes the current track by additionaly binding to the controllers scope yesDelete() method. This additional binding was added to the directive scope with  the '&' attribute.

--- client/components/areyousureModal/sureModal.directive.js
+++ client/components/areyousureModal/sureModal.directive.js
@@ -5,9 +5,16 @@ angular.module('mixtapes')
 .directive('suremodal', function() {
   return {
     restrict: "EA",
-    scope: {},
-    template: '<button ng-init"modalShow = false" ng-click="modalShow = !modalShow"/> Toggle'+ '<div>' +
-    '<h3> modalShow directive is {{modalShow}}</h3>'+ '</div>'
+    scope: {
+      remove:'&'
+    },
+    template: '<button ng-init"modalShow = false" ng-click="modalShow = !modalShow"/> Delete'+ '<div>' +
+    '<div ng-show="modalShow" class="m-ok-modal">' +
+    '<h3> Are you sure you want to delete this mix ? </h3>' +
+    '<button type="button" ng-click="remove(track)" /> Yes'  +
+    '<button type="button" ng-click="modalShow = !modalShow"/> Cancel' +
+    '</div>' +
+    '</div>'
   }//end return
 })
 
@@ -19,3 +26,7 @@ angular.module('mixtapes')
 // '<button type="button" ng-click="hideModal(track)"/> Cancel' +
 // '</div>' +
 // '</div>'
+
+
+// '<button ng-init"modalShow = false" ng-click="modalShow = !modalShow"/> Toggle'+ '<div>' +
+// '<h3> modalShow directive is {{modalShow}}</h3>'+ '</div>'
