fb22b29bd273388b23e12aec2ac4807aa1b8d692
EricSSartorius <EricSSartorius@gmail.com>
2016-03-15 23:34:54 -0400

    homepage animations finished

--- client/controllers/mainCtrl.ng.js
+++ client/controllers/mainCtrl.ng.js
@@ -7,11 +7,10 @@ function mainCtrl($scope){
 	window.onload = function() {
 		var introText = document.getElementsByClassName('intro-text')[0];
 
-		//Animate text in hero on load
+		//Animate top text on load
 		introText.className += ' is-showing';
 
 		window.addEventListener("scroll", function(event) {
-			var wScroll = document.body.scrollTop;
 			var botLeft = document.getElementsByClassName('bot-left-intro')[0];
 			var botRight = document.getElementsByClassName('bot-right-intro')[0];
 			var midLeft = document.getElementsByClassName('mid-left-intro')[0];
@@ -20,15 +19,16 @@ function mainCtrl($scope){
 			var mid = document.getElementsByClassName('intro-mid')[0];
 			var btn = document.getElementsByClassName('btn')[0];
 			var logo = document.getElementsByClassName('spotify-logo')[0];
-		 	console.log(wScroll);
+			var offset = bot.getBoundingClientRect().top - window.innerHeight + 350;
 
-	       	//Animate  text on scroll
+	       	//Animate  middle text on scroll
 	       	if(document.documentElement.scrollTop || midLeft.getBoundingClientRect().top - window.innerHeight < -50) {
 	       		midLeft.classList.add('is-showing');
 			}
 			if(document.documentElement.scrollTop || midRight.getBoundingClientRect().top - window.innerHeight < -100) {
 				midRight.classList.add('is-showing');
 			}
+			//Animate  bottom text on scroll
 			if(document.documentElement.scrollTop || botLeft.getBoundingClientRect().top - window.innerHeight < -50) {
 	            botLeft.classList.add('is-showing');
 			  	botRight.classList.add('is-showing'); 
@@ -36,17 +36,24 @@ function mainCtrl($scope){
 			if(document.documentElement.scrollTop || btn.getBoundingClientRect().top - window.innerHeight < -50) {
 				btn.classList.add('is-showing');
 			}
-
-			//Move triangles in on scroll
-			if(document.documentElement.scrollTop || mid.getBoundingClientRect().top - window.innerHeight < 0) {
-
-			}
-			console.log(wScroll - document.body.clientHeight);
-			//Move logo up on scroll
+			
+			//Move Spotify logo up on scroll
 			if(document.documentElement.scrollTop || bot.getBoundingClientRect().top - window.innerHeight < 0) {
-				logo.style.transform = 'translate(0px, -'+ wScroll /4 +'%)';
-				// console.log(logo.getBoundingClientRect().top - window.innerHeight);
-				// console.log('triggered');
+				if(window.innerWidth > 700) {
+					logo.style.transform = 'translate(0px, '+ offset / 1.2 +'px)';
+				}
+				else if(window.innerWidth <= 700 && window.innerWidth > 640) {
+					logo.style.transform = 'translate(0px, '+ (offset + 100) / 1.2 +'px)';
+				}
+				else if(window.innerWidth <= 640 && window.innerWidth > 460){
+					logo.style.transform = 'translate(0px, '+ (offset + 20) / 1.2 +'px)';
+				}
+				else if(window.innerWidth <= 460 && window.innerWidth > 450){
+					logo.style.transform = 'translate(0px, '+ (offset + 30) / 1.2 +'px)';
+				}
+				else {
+					logo.style.transform = 'translate(0px, -200px)';
+				}
 			}
     	});
 	}
